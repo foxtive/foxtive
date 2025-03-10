8e5861a0817efcfe4aa6d6a8163b44bc40b7a5cb
ahmard <ahmard.mu@gmail.com>
2025-03-10 11:43:03 +0100

    feat(json): add response structs

--- foxtive/src/helpers/json.rs
+++ foxtive/src/helpers/json.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter, Write};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -5,4 +6,37 @@ pub struct JsonEmpty {}
 
 pub fn json_empty() -> JsonEmpty {
     JsonEmpty {}
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct JsonResponse<T: Serialize> {
+    pub code: String,
+    pub success: bool,
+    pub timestamp: u64,
+    pub message: Option<String>,
+    pub data: T,
+}
+
+#[derive(Debug, Serialize)]
+pub struct SeJsonResponse<T> {
+    pub code: String,
+    pub success: bool,
+    pub timestamp: u64,
+    pub message: Option<String>,
+    pub data: T,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DeJsonResponse<T> {
+    pub code: String,
+    pub success: bool,
+    pub timestamp: u64,
+    pub message: Option<String>,
+    pub data: T,
+}
+
+impl<T: Serialize> Display for JsonResponse<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(serde_json::to_string(self).unwrap().as_str())
+    }
 }
