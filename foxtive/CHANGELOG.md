36a2ea5b08a5b00f38e48b4607ce094b2c424a17
ahmard <ahmard.mu@gmail.com>
2025-03-11 14:02:36 +0100

    feat(app-result): added `AppResult<T>.map_app_msg(|m| m)`

--- foxtive/CHANGELOG.md
+++ foxtive/CHANGELOG.md
@@ -1,6 +1,9 @@
 # Foxtive Changelog
 Foxtive changelog file 
 
+### 0.4.1
+* Added `AppResult<T>.map_app_msg(|m| m)`
+
 ### 0.4.0
 * Added database shareable ext
 * Move database traits to 'ext' folder
--- foxtive/src/results/app_result.rs
+++ foxtive/src/results/app_result.rs
@@ -1,4 +1,5 @@
 use crate::helpers::json::{json_empty, JsonEmpty};
+use crate::prelude::AppMessage;
 use crate::results::AppResult;
 #[cfg(feature = "database")]
 use diesel::result::Error;
@@ -14,6 +15,24 @@ pub trait IntoEmptyJson {
     fn into_empty_json(self) -> AppResult<JsonEmpty>;
 }
 
+pub trait MapAppMessage<T> {
+    fn map_app_msg<F>(self, func: F) -> AppResult<T>
+    where
+        F: FnOnce(AppMessage) -> AppMessage;
+}
+
+impl<T> MapAppMessage<T> for AppResult<T> {
+    fn map_app_msg<F>(self, func: F) -> AppResult<T>
+    where
+        F: FnOnce(AppMessage) -> AppMessage,
+    {
+        self.map_err(|err| match err.downcast::<AppMessage>() {
+            Err(err) => err,
+            Ok(message) => func(message).ae(),
+        })
+    }
+}
+
 impl<T: Serialize> IntoEmptyJson for AppResult<T> {
     fn into_empty_json(self) -> AppResult<JsonEmpty> {
         Ok(json_empty())
@@ -26,7 +45,7 @@ impl<T> IntoAppResult<T> for QueryResult<T> {
         match self {
             Ok(value) => Ok(value),
             Err(Error::NotFound) => {
-                Err(crate::prelude::AppMessage::EntityNotFound("".to_string()).into())
+                Err(AppMessage::EntityNotFound("".to_string()).into())
             }
             Err(e) => Err(e.into()),
         }
