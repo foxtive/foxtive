4418cab5de49ba95cd1f7114509200c4d06db03f
ahmard <ahmard.mu@gmail.com>
2025-04-02 18:40:29 +0100

    feat(app-result): 'recover_from_async' to recover error from Error or AppResult<T>

--- foxtive/src/ext.rs
+++ foxtive/src/ext.rs
@@ -1,3 +1,4 @@
+use std::future::Future;
 use crate::prelude::AppMessage;
 use crate::results::AppResult;
 
@@ -5,6 +6,11 @@ pub trait RecoverAppResultExt<T> {
     fn recover_from<F>(self, func: F) -> AppResult<T>
     where
         F: FnOnce(AppMessage) -> AppResult<T>;
+
+    fn recover_from_async<F, Fut>(self, func: F) -> impl Future<Output = AppResult<T>> + Send
+    where
+        F: FnOnce(AppMessage) -> Fut + Send,
+        Fut: Future<Output = AppResult<T>> + Send;
 }
 
 pub trait AppErrorExt {
--- foxtive/src/ext_impl.rs
+++ foxtive/src/ext_impl.rs
@@ -1,8 +1,9 @@
 use crate::ext::{AppErrorExt, RecoverAppResultExt};
 use crate::prelude::{AppMessage, AppResult};
 use crate::Error;
+use std::future::Future;
 
-impl<T> RecoverAppResultExt<T> for AppResult<T> {
+impl<T: Send> RecoverAppResultExt<T> for AppResult<T> {
     fn recover_from<F>(self, func: F) -> AppResult<T>
     where
         F: FnOnce(AppMessage) -> AppResult<T>,
@@ -11,6 +12,20 @@ impl<T> RecoverAppResultExt<T> for AppResult<T> {
             Ok(val) => Ok(val),
             Err(err) => match err.downcast::<AppMessage>() {
                 Ok(message) => func(message),
+                Err(err) => Err(err),
+            },
+        }
+    }
+
+    async fn recover_from_async<F, Fut>(self, func: F) -> AppResult<T>
+    where
+        F: FnOnce(AppMessage) -> Fut + Send,
+        Fut: Future<Output = AppResult<T>> + Send,
+    {
+        match self {
+            Ok(val) => Ok(val),
+            Err(err) => match err.downcast::<AppMessage>() {
+                Ok(message) => func(message).await,
                 Err(err) => Err(err),
             },
         }
@@ -24,6 +39,17 @@ impl<T> RecoverAppResultExt<T> for Error {
     {
         match self.downcast::<AppMessage>() {
             Ok(message) => func(message),
+            Err(err) => Err(err),
+        }
+    }
+
+    async fn recover_from_async<F, Fut>(self, func: F) -> AppResult<T>
+    where
+        F: FnOnce(AppMessage) -> Fut + Send,
+        Fut: Future<Output = AppResult<T>> + Send,
+    {
+        match self.downcast::<AppMessage>() {
+            Ok(message) => func(message).await,
             Err(err) => Err(err),
         }
     }
@@ -60,6 +86,31 @@ mod tests {
             assert_eq!(err.message(), "User created");
             Ok("recovered".to_string())
         });
+        assert_eq!(result.unwrap(), "recovered");
+    }
+
+    #[tokio::test]
+    async fn test_recover_from_async_error() {
+        let result = AppMessage::InternalServerError
+            .ae()
+            .recover_from_async(|err| {
+                assert_eq!(err.status_code(), StatusCode::INTERNAL_SERVER_ERROR);
+                assert_eq!(err.message(), "Internal Server Error");
+                async { Ok("recovered".to_string()) }
+            })
+            .await;
+        assert_eq!(result.unwrap(), "recovered");
+    }
+
+    #[tokio::test]
+    async fn test_recover_from_async_result() {
+        let result = Err(AppMessage::SuccessMessage("User created").ae())
+            .recover_from_async(|err| {
+                assert_eq!(err.status_code(), StatusCode::OK);
+                assert_eq!(err.message(), "User created");
+                async { Ok("recovered".to_string()) }
+            })
+            .await;
         assert_eq!(result.unwrap(), "recovered");
     }
 
