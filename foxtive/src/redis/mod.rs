f174b211b06adfcb44389a81acdecf1170b45a0e
GitHub Action <action@github.com>
2026-06-04 20:11:07 +0000

    style(cargo-fmt): apply changes

--- foxtive/src/redis/mod.rs
+++ foxtive/src/redis/mod.rs
@@ -1,7 +1,7 @@
+use crate::FOXTIVE;
 use crate::prelude::{AppResult, AppStateExt};
 use crate::redis::conn::create_redis_connection;
 use crate::results::redis_result::RedisResultToAppResult;
-use crate::FOXTIVE;
 use anyhow::Error;
 use futures_util::StreamExt;
 use redis::{AsyncCommands, FromRedisValue, ToRedisArgs, ToSingleRedisArg};
